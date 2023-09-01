4d733b4481ea29a4a1bebb32f466f2de23ce89bf
marcusdograu088 <zubuff@gmail.com>
2023-09-01 17:00:31 -0300

    finalizado o projeto

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -9,19 +9,20 @@ import MovieCard from "../componentes/movieCard/movieCard";
 import Footer from "../componentes/footer/footer";
 import MovieCards from "../componentes/movieCards/movieCards";
 
-
 const App = () => {
   // Declaração do componente funcional 'App'
-  
+
   const [searchTerm, setSearchTerm] = useState(""); // Declara o estado 'searchTerm' e a função 'setSearchTerm' para atualizá-lo
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [emAlta, setEmAlta] = useState([]);
+
   const apiKey = "927ee5bf"; // Declara uma chave de API
   const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`; // Monta a URL da API com a chave de API
 
-  
   useEffect(() => {
     // Utiliza o hook 'useEffect'
-    searchMovies("batman"); // Chama a função 'searchMovies' com o argumento "Batman" quando o componente é montado
+    searchMovies("batman");
+    searchEmAlta("barbie"); // Chama a função 'searchMovies' com o argumento "Batman" quando o componente é montado
   }, []);
 
   const searchMovies = async (title) => {
@@ -32,6 +33,15 @@ const App = () => {
     const data = await response.json(); // Converte a resposta da requisição em formato JSON
     console.log(data);
     setMovies(data.Search);
+  };
+  const searchEmAlta = async (title) => {
+    // Declaração da função assíncrona 'searchMovies' com o argumento 'title'
+
+    const response = await fetch(`${apiUrl}&s=${title}`); // Faz uma requisição assíncrona para a API usando a URL construída
+
+    const data = await response.json(); // Converte a resposta da requisição em formato JSON
+    console.log(data);
+    setEmAlta(data.Search);
   };
   const handleKeyPress = (e) => {
     // Declaração da função 'handleKeyPress' com o argumento 'e' (evento)
@@ -50,12 +60,11 @@ const App = () => {
       <div id="my-image">
         <img src="src/assets/devf.png"></img>
       </div>
-      <div className="logo">
-        {/* Div para exibir o logo */} <img src={logo} alt="logo devflix" />
-        {/* Exibe a imagem do logo importada */}
-      </div>
-     
-     
+      <div className="flexLogo">
+        <div className="logo">
+          {/* Div para exibir o logo */} <img src={logo} alt="logo devflix" />
+          {/* Exibe a imagem do logo importada */}
+        </div>
         <div className="search">
           {/* Div para a barra de pesquisa */}
           <input
@@ -78,8 +87,8 @@ const App = () => {
             onClick={() => searchMovies(searchTerm)}
           ></ion-icon>
           {/*Chama a função 'searchMovies' com o valor de 'searchTerm' quando a imagem é clicada */}
+        </div>
       </div>
-     
       {movies?.length > 0 && (
         <div className="flex">
           <div className="letrastl">
@@ -90,11 +99,11 @@ const App = () => {
           </div>
         </div>
       )}
-      {movies?.length > 0 ? ( // Início de uma expressão JSX condicional baseada no tamanho da matriz 'movies'
+      {emAlta?.length > 0 ? ( // Início de uma expressão JSX condicional baseada no tamanho da matriz 'movies'
         <div className="container">
           {/* Abre uma div com a classe CSS "container" */}
           <section className="sectionmasterpgninicial">
-            {movies.map(
+            {emAlta.map(
               (
                 movie /* Renderiza um componente 'MovieCard' passando 'movie' como prop */
               ) => (
@@ -115,18 +124,16 @@ const App = () => {
           {/* Renderiza um cabeçalho indicando que nenhum filme foi encontrado */}
         </div> // Fecha a div do 'empty'
       )}
-
       {movies?.length > 0 && (
         <div className="flex">
           <div className="letrastl">
             <h1>Recomend</h1>
           </div>
-            <div className="ionsl">
-              <ion-icon name="play-outline" size="large"></ion-icon>
-            </div>
+          <div className="ionsl">
+            <ion-icon name="play-outline" size="large"></ion-icon>
+          </div>
         </div>
       )}
-
       {movies?.length > 0 ? ( // Início de uma expressão JSX condicional baseada no tamanho da matriz 'movies'
         <div className="containers">
           {/* Abre uma div com a classe CSS "container" */}
